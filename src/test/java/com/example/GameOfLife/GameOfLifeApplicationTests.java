df8ba950372983474518c8753b799ec365da55b5
ak1386 <nileshnaik1386@gmail.com>
2020-02-23 19:00:46 +0530

    coderefactor

---
+++ src/test/java/com/example/GameOfLife/GameOfLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GameOfLife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameOfLifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
