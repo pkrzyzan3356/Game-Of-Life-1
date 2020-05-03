6c80970aa14764c2aded479331d25da58edfb9c9
pkrzyzan3356 <34374479+pkrzyzan3356@users.noreply.github.com>
2020-05-03 20:07:24 +0200

    Character # fixed

--- src/main/java/com/example/GameOfLife/GameOfLifeApplication.java
+++ src/main/java/com/example/GameOfLife/GameOfLifeApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GameOfLifeApplication {
 
-	public static void main(String[#] args) {
+	public static void main(String[] args) {
 		SpringApplication.run(GameOfLifeApplication.class, args);
 	}
 
